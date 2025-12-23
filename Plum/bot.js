fc064fe74accdf94d4489ea12a5337a98a15af13
Jardin <84980243+jardinsys@users.noreply.github.com>
2025-12-22 23:53:22 -0600

    slash command handling init

--- Plum/bot.js
+++ Plum/bot.js
@@ -1,3 +1,5 @@
+const fs = require('node:fs');
+const path = require('node:path');
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const config = require('./../config.json');
 const token = config.discordTokens.prune;
@@ -11,3 +13,46 @@ client.once(Events.ClientReady, (readyClient) => {
 
 // Log in to Discord
 client.login(token);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+//Interaction Handling
+client.on(Events.InteractionCreate, async (interaction) => {
+	if (!interaction.isChatInputCommand()) return;
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({
+				content: 'There was an error while executing this command!',
+				flags: MessageFlags.Ephemeral,
+			});
+		} else {
+			await interaction.reply({
+				content: 'There was an error while executing this command!',
+				flags: MessageFlags.Ephemeral,
+			});
+		}
+	}
+});
